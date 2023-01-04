6648ad2193456b287993a11b6c57facac1b01fe0
H4Y6 <h4y6@i.ua>
2023-01-04 23:41:27 +0200

    new file:   2/commander-example/.gitignore new file:   2/commander-example/app.js new file:   2/commander-example/books/books.json new file:   2/commander-example/books/index.js new file:   2/commander-example/package-lock.json new file:   2/commander-example/package.json new file:   2/process.argv-example/.gitignore new file:   2/process.argv-example/app.js new file:   2/process.argv-example/books/books.json new file:   2/process.argv-example/books/index.js new file:   2/process.argv-example/package-lock.json new file:   2/process.argv-example/package.json new file:   2/yargs-example/.gitignore new file:   2/yargs-example/app.js new file:   2/yargs-example/books/books.json new file:   2/yargs-example/books/index.js new file:   2/yargs-example/package-lock.json new file:   2/yargs-example/package.json

---
+++ 2/commander-example/app.js
@@ -0,0 +1,50 @@
+const { program } = require("commander");
+
+const books = require("./books");
+
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "getAll":
+      const allBooks = await books.getAll();
+      console.log(allBooks);
+      break;
+    case "getById":
+      const idBook = await books.getById(id);
+      console.log(idBook);
+      break;
+    case "add":
+      const addBook = await books.add(title, author);
+      console.log(addBook);
+      break;
+    case "updateById":
+      const updateBook = await books.updateById(id, title, author);
+      console.log(updateBook);
+      break;
+    case "deleteById":
+      const deleBooks = await books.deleteById(id);
+      console.log(deleBooks);
+      break;
+  }
+};
+
+// invokeAction({action: 'getAll'})
+// invokeAction({ action: "getById", id: "8a" });
+// invokeAction({ action: "add", title: "Oscar", author: "Ben Strap" });
+// invokeAction({
+//   action: "updateById",
+//   id: "pJ2k3SY7LanxXNdZ-7pjf",
+//   title: "Oscar",
+//   author: "Benjamin Satrap",
+// });
+// invokeAction({ action: "deleteById", id: "pJ2k3SY7LanxXNdZ-7pjf" });
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-t, --title <type>")
+  .option("-at, --author <type>");
+
+program.parse(process.argv);
+const options = program.opts();
+// console.log(options);
+invokeAction(options);
---
+++ 2/yargs-example/app.js
@@ -0,0 +1,46 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const books = require("./books");
+
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "getAll":
+      const allBooks = await books.getAll();
+      console.log(allBooks);
+      break;
+    case "getById":
+      const idBook = await books.getById(id);
+      console.log(idBook);
+      break;
+    case "add":
+      const addBook = await books.add(title, author);
+      console.log(addBook);
+      break;
+    case "updateById":
+      const updateBook = await books.updateById(id, title, author);
+      console.log(updateBook);
+      break;
+    case "deleteById":
+      const deleBooks = await books.deleteById(id);
+      console.log(deleBooks);
+      break;
+  }
+};
+
+// invokeAction({action: 'getAll'})
+// invokeAction({ action: "getById", id: "8a" });
+// invokeAction({ action: "add", title: "Oscar", author: "Ben Strap" });
+// invokeAction({
+//   action: "updateById",
+//   id: "pJ2k3SY7LanxXNdZ-7pjf",
+//   title: "Oscar",
+//   author: "Benjamin Satrap",
+// });
+// invokeAction({ action: "deleteById", id: "pJ2k3SY7LanxXNdZ-7pjf" });
+
+const arr = hideBin(process.argv);
+// console.log(arr);
+const { argv } = yargs(arr);
+// console.log(argv);
+invokeAction(argv);
