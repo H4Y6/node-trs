f909fc1a4fbfcda50dcf5a5cb3bff6c5829a02c8
H4Y6 <h4y6@i.ua>
2023-01-07 21:34:27 +0200

    modified:   3/express-example-1/app.js

--- 3/express-example-1/app.js
+++ 3/express-example-1/app.js
@@ -1,21 +1,22 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express(); // app - веб-сервер
+const app = express(); // app - веб-сервер
 // // GET /contacts
 // // GET /
-// app.get("/", (request, response)=> {
-//     console.log(request.method);
-//     console.log(request.url);
-//     response.send("<h2>Home page</h2>");
-// });
+app.get("/", (request, response) => {
+  console.log(request.method);
+  console.log(request.url);
+  response.send("<h2>Home page</h2>");
+});
 
-// app.get("/contacts", (request, response)=> {
-//     console.log(request.method);
-//     console.log(request.url);
-//     response.send("<h2>Contacts page</h2>")
-// })
+app.get("/contacts", (request, response) => {
+  console.log(request.method);
+  console.log(request.url);
+  response.send("<h2>Contacts page</h2>");
+});
 
-// app.listen(3000, ()=> console.log("Server running"));
-const express = require("express");
-const app = express();
-app.listen(3000, () => console.log("Listening to port 3000"));
+app.listen(3000, () => console.log("Server running"));
+
+// const express = require("express");
+// const app = express();
+// app.listen(3000, () => console.log("Listening to port 3000"));
