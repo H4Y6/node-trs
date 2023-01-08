977cbf3ca960ec0e465730f2f74e6dac131935dc
H4Y6 <h4y6@i.ua>
2023-01-08 19:59:50 +0200

    add file:   3/express-2/package-lock.json modified:   3/express-2/package.json

--- 3/express-2/app.js
+++ 3/express-2/app.js
@@ -1,37 +1,27 @@
 const express = require("express");
-const cors = require("cors");
-// const fs = require("fs/promises");
-// const moment = require("moment");
-
-const books = require("./books");
-
+const fs = require("fs/promises");
+const moment = require("moment");
 const app = express();
 
-app.use(cors());
+const books = require("./books");
 
 app.use(async (req, res, next) => {
   const { method, url } = req;
   const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-  await fs.appendFile("server.log", `\n${method} ${url} ${date}`);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Second middleware");
+  // const date = new Date().toString().slice(0, 25);
+  await fs.appendFile("server.log", `${method} ${url} ${date}\n`);
   next();
 });
 
 app.get("/books", (req, res) => {
   res.json(books);
+  // res.json(null);
+  // res.send(books);
+  // res.send(null);
 });
 
 app.get("/products", (req, res) => {
   res.json([]);
 });
 
-app.listen(3000);
+app.listen(3000, console.log("3000"));
