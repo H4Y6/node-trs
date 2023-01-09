788b809074a9d392a398dfd0866b8476c0a24c5e
H4Y6 <h4y6@i.ua>
2023-01-09 21:38:10 +0200

    modified:   3/express-3/app.js 	modified:   3/express-3/data/books.js 	modified:   3/express-3/package-lock.json 	modified:   3/express-3/package.json 	modified:   3/express-3/routes/api/books.js 	deleted:    3/express-3a/.gitignore 	deleted:    3/express-3a/app.js 	deleted:    3/express-3a/data/books.js 	deleted:    3/express-3a/package-lock.json 	deleted:    3/express-3a/package.json 	deleted:    3/express-3a/routes/api/books.js

--- 3/express-3/app.js
+++ 3/express-3/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs/promises");
 
 const booksRouter = require("./routes/api/books");
 
 const app = express();
+
+app.use(async (req, res, next) => {
+  const { method, url } = req;
+  const date = new Date().toString().slice(0, 25);
+  await fs.appendFile("server.log", `${method} ${url} ${date}\n`);
+  next();
+});
 
 app.use(cors());
 
@@ -13,4 +21,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Use port 3000");
+});
--- 3/express-3/routes/api/books.js
+++ 3/express-3/routes/api/books.js
@@ -9,7 +9,6 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {});
-
 router.post("/", (req, res) => {});
 
 module.exports = router;
