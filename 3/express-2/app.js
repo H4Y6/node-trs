3a5cc078380ab1473bad5ce66fe3bfec71dca99b
H4Y6 <h4y6@i.ua>
2023-01-10 11:59:06 +0200

    modified:   3/express-2/app.js

--- 3/express-2/app.js
+++ 3/express-2/app.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs/promises");
-const moment = require("moment");
-const app = express();
 
-const books = require("./books");
+const moment = require("moment");
+
+const books = require("./data/books");
+
+const app = express();
 
 app.use(cors());
 
 app.use(async (req, res, next) => {
   const { method, url } = req;
-  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-  // const date = new Date().toString().slice(0, 25);
-  await fs.appendFile("server.log", `${method} ${url} ${date}\n`);
+  const date = new Date().toString().slice(0, 25);
+  // const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  await fs.appendFile("./server.log", `${method} ${url} ${date}\n`);
   next();
 });
 
 app.get("/books", (req, res) => {
   res.json(books);
-  // res.json(null);
-  // res.send(books);
-  // res.send(null);
 });
-
 app.get("/products", (req, res) => {
   res.json([]);
 });
 
-app.listen(3000, console.log("3000"));
+app.listen(3000, () => {
+  console.log("Listen to port: 3000");
+});
