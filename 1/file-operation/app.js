9703feca8cd8010e907b5e4e4956915bf25c3072
H4Y6 <h4y6@i.ua>
2023-01-10 19:29:24 +0200

    modified:   1/file-operation/app.js

--- 1/file-operation/app.js
+++ 1/file-operation/app.js
@@ -1,7 +1,5 @@
 // const fs = require("fs");
 
-const { error } = require("console");
-
 // fs.readFile("./files/file.txt", "utf-8", (error, data) => {
 //   console.log(error);
 //   console.log(data);
@@ -23,11 +21,33 @@ const fs = require("fs/promises");
 const fileOperation = async ({ filePath, action, data }) => {
   switch (action) {
     case "read":
-      const data = await fs.readFile(filePath);
-      const text = data.toString();
+      // const data = await fs.readFile(filePath);
+      // const text = data.toString();
+      const text = await fs.readFile(filePath, "utf8");
       console.log(text);
+      break;
+    case "add":
+      await fs.appendFile(filePath, data);
+      break;
+    case "replace":
+      await fs.writeFile(filePath, data);
       break;
   }
 };
 
 fileOperation({ filePath: "./files/file.txt", action: "read" });
+// fileOperation({
+//   filePath: "./files/file.txt",
+//   action: "add",
+//   data: "\nBe happy!",
+// });
+// fileOperation({
+//   filePath: "./files/file.txt",
+//   action: "replace",
+//   data: "Hello!!",
+// });
+// fileOperation({
+//   filePath: "./files/file-a.txt",
+//   action: "replace",
+//   data: "Hi!",
+// });
