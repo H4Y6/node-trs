59619ea13dd3052254d83f717f4d2cf12fd063a2
H4Y6 <h4y6@i.ua>
2023-01-12 12:54:21 +0200

    modified:   4/routes/api/books.js

--- 4/routes/api/books.js
+++ 4/routes/api/books.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 
 const books = require("../../models/books");
@@ -8,6 +9,20 @@ router.get("/", async (req, res, next) => {
   try {
     const result = await books.getAll();
     res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    console.log(req.params);
+    const id = req.params;
+    const idBook = await books.getById(id);
+    if (!idBook) {
+      res.status(404).json({ message: "Not found" });
+    }
+    res.json(idBook);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
