6f70085579cb9362f208d6e79fd8bfae3c17976a
H4Y6 <h4y6@i.ua>
2023-01-16 21:43:10 +0200

    modified:   4/app.js, 4/routes/api/books.js 	new file:   4/helpers/createError.js, 4/helpers/index.js

--- 4/app.js
+++ 4/app.js
@@ -1,21 +1,57 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+// const express = require("express");
+// const logger = require("morgan");
+// const cors = require("cors");
 
+// const booksRouter = require("./routes/api/books");
+
+// const app = express();
+
+// const formatLogger = app.get("env") === "development" ? "dev" : "short";
+
+// app.use(logger(formatLogger));
+
+// app.use(cors());
+
+// app.use("/api/books", booksRouter);
+
+// app.use((req, res) => {
+//   res.status(404).json({ messagr: "Not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
+
+// module.exports = app;
+
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 const booksRouter = require("./routes/api/books");
+const { json } = require("express");
 
 const app = express();
-
 const formatLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatLogger));
 
 app.use(cors());
-
 app.use("/api/books", booksRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ messagr: "Not found" });
+  res.status(404).json({ message: "Not found" });
+});
+
+// app.use(err, req, res, next) => {
+app.use((err, _, res, __) => {
+  // const { status = 500, message = "Server error" } = err;
+  // res.status(status).json({ message });
+
+  // res.status(500).json({ message: err.message });
+
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
 module.exports = app;
--- 4/routes/api/books.js
+++ 4/routes/api/books.js
@@ -1,6 +1,38 @@
-const books = require("../../models/books");
+// const express = require("express");
+// const books = require("../../models/books");
+// const router = express.Router();
+// const { createError } = require("../../helpers");
+
+// router.get("/", async (req, res, next) => {
+//   try {
+//     const result = await books.getAll();
+//     res.status(200).json(result);
+//   } catch (error) {
+//     res.json({ message: "Server error" });
+//   }
+// });
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await books.getById(id);
+//     // return result
+//     //   ? res.status(200).json(result)
+//     //   : res.status(404).json({ message: "Not found" });
+//     if (!result) {
+//       // const error = new Error("Not found");
+//       // error.status(404);
+//       // throw error;
+//       throw createError(404);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 const express = require("express");
+const books = require("../../models/books");
+const { createError } = require("../../helpers");
 
 const router = express.Router();
 
@@ -9,18 +41,28 @@ router.get("/", async (req, res, next) => {
     const result = await books.getAll();
     res.status(200).json(result);
   } catch (error) {
-    return res.json({ message: "Server error" });
+    // res.status(500).json({ message: "Not found" });
+    next(error);
   }
 });
+
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await books.getById(id);
-    result
-      ? res.status(200).json(result)
-      : res.status(404).json({ message: "Not found" });
+    if (!result) {
+      // return res.status(404).json({ message: "Not found" });
+
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error;
+
+      throw createError(404, "Not found");
+    }
+    res.json(result);
   } catch (error) {
-    return res.json({ message: "Server error" });
+    // res.status(500).json({ message: "Server error" });
+    next(error);
   }
 });
 
