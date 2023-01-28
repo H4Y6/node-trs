e2e2ca69bf37638f6abe01c11049b53c8a25fa3a
H4Y6 <h4y6@i.ua>
2023-01-28 10:57:57 +0200

    modified:   3-rest-api/models/contacts/index.js

--- 3-rest-api/models/contacts/index.js
+++ 3-rest-api/models/contacts/index.js
@@ -1,3 +1,13 @@
-const contacts = require("./contacts.json");
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-module.exports = contacts;
+const contacts = require("./contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+module.exports = { listContacts };
