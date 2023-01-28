e4fa30e9691f51db88d44d4e2deddf7916992a7c
H4Y6 <h4y6@i.ua>
2023-01-28 17:50:08 +0200

    modified:   3-rest-api/app.js, new file:   3-rest-api/helpers/createError.js new file:   3-rest-api/helpers/index.js modified:   3-rest-api/models/contacts/contacts.json modified:   3-rest-api/package-lock.json modified:   3-rest-api/package.json modified:   3-rest-api/routes/api/contacts.js

--- 3-rest-api/app.js
+++ 3-rest-api/app.js
@@ -11,5 +11,12 @@ app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 module.exports = app;
--- 3-rest-api/routes/api/contacts.js
+++ 3-rest-api/routes/api/contacts.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
 const contacts = require("../../models/contacts");
+const { createError } = require("../../helpers");
+
+const contactsAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -16,7 +24,11 @@ router.get("/:id", async (req, res, next) => {
     const { id } = req.params;
     const result = await contacts.getContactById(id);
     if (!result) {
-      return res.status(404).json({ message: "Not found" });
+      // return res.status(404).json({ message: "Not found" });
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error;
+      throw createError(404);
     }
     res.json(result);
   } catch (error) {
@@ -26,6 +38,12 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    const { error } = contactsAddSchema.validate(req.body);
+    if (error) {
+      const error = new Error("Wrong requst body");
+      error.status = 400;
+      throw error;
+    }
     const { name, email, phone } = req.body;
     const result = await contacts.addContact(name, email, phone);
     res.status(201).json(result);
