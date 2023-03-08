67945ef13e9192dd8bf4cf499e31cbb8eb446270
H4Y6 <h4y6@i.ua>
2023-03-08 18:28:57 +0200

    modified:   al-lut-01/index.html,   al-lut-01/index.js

--- al-lut-01/index.js
+++ al-lut-01/index.js
@@ -1,22 +1,24 @@
 const date = new Date();
-const getCurrentTime = () => {
-  const currentTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `;
-  return currentTime;
-};
+// const getCurrentTime = () => {
+//   const currentTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `;
+//   return currentTime;
+// };
 
-const getCurrentDate = () => {
-  const currentDate = `${date.getDate()}.${
-    date.getMonth() + 1
-  }.${date.getFullYear()}`;
-  return currentDate;
-};
+// const getCurrentDate = () => {
+//   const currentDate = `${date.getDate()}.${
+//     date.getMonth() + 1
+//   }.${date.getFullYear()}`;
+//   return currentDate;
+// };
 
 getCurrentMoment = () => {
   const currentMoment = date.toString().slice(0, 25);
   return currentMoment;
 };
-document.getElementById("time").innerHTML = "Time is: " + getCurrentTime();
-document.getElementById("date").innerHTML = "Today is: " + getCurrentDate();
-document.getElementById("moment").innerHTML = getCurrentMoment();
+// document.getElementById("time").innerHTML = "Time is: " + getCurrentTime();
+// document.getElementById("date").innerHTML = "Today is: " + getCurrentDate();
+const now = document.getElementById("moment");
+now.innerHTML = getCurrentMoment();
+now.style.textAlign = "center";
 
 console.log(getCurrentTime(), getCurrentDate());
