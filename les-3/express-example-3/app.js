d43152125143f24d0a468fd7629fa5ddf3238b17
H4Y6 <h4y6@i.ua>
2023-03-14 11:30:39 +0200

    modified:   les-3/express-example-3/app.js

--- les-3/express-example-3/app.js
+++ les-3/express-example-3/app.js
@@ -7,27 +7,20 @@ const app = express();
 
 app.use(async (req, res, next) => {
   const { method, url } = req;
-  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-  await fs.appendFile("./server.log", `${method} ${url} ${date}\n`);
+  // const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  const date = new Date().toString().slice(0, 25);
+  await fs.appendFile("./server.log", `${date} ${method} ${url}\n`);
   next();
 });
 
 app.use(cors());
-// app.use(async (req, res, next) => {
-//   console.log("First middleware");
-//   next();
-// });
-// app.use(async (req, res, next) => {
-//   console.log("Second middleware");
-//   next();
-// });
 
 app.get("/books", (req, res) => {
   res.json(books);
 });
 app.get("/products", (req, res) => {
-  console.log(req.method);
-  console.log(req.url);
+  // console.log(req.method);
+  // console.log(req.url);
   res.json([]);
 });
 
