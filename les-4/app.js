336b71a88fb81739e7bf658699173e6c3711d989
H4Y6 <h4y6@i.ua>
2023-03-14 13:40:00 +0200

    new dir:   les-4

---
+++ les-4/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const booksRouter = require("./routes/api/books");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(logger(formatsLogger));
+
+app.use(cors());
+app.use("/api/books", booksRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
---
+++ les-4/server.js
@@ -0,0 +1,5 @@
+const app = require("./app  ");
+
+app.listen(3000, () => {
+  console.log("Use port: 30000");
+});
