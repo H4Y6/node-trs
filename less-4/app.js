2bd2b2e228b0adf6f8f1ef0eea31d3e8d5df1898
H4Y6 <h4y6@i.ua>
2023-03-16 13:32:24 +0200

    modified:   less-4/app.js, less-4/router/api/books.js

--- less-4/app.js
+++ less-4/app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 const booksRouter = require("./router/api/books");
-const { json } = require("express");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-app.use(logger.formatsLogger);
+app.use(logger(formatsLogger));
 app.use(cors());
 
 app.use("/api/books", booksRouter);
@@ -18,10 +17,6 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
-});
-
-app.listen(3000, () => {
-  console.log("Use port: 3000 ");
 });
 
 module.exports = app;
--- less-4/router/api/books.js
+++ less-4/router/api/books.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const books = require("../../models/books");
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  const result = await books.getAll();
+  res.json(result);
+});
 
 module.exports = router;
