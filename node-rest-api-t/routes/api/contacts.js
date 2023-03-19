89d4e1c02a05c896fd876d2c6f0e3ec0d1908db1
H4Y6 <h4y6@i.ua>
2023-03-19 16:39:51 +0200

    node-rest-api-t/routes/api/contacts.js

--- node-rest-api-t/routes/api/contacts.js
+++ node-rest-api-t/routes/api/contacts.js
@@ -18,7 +18,9 @@ router.get("/:contactId", async (req, res, next) => {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
     if (!result) {
-      return res.status(404).json({ message: "Not found" });
+      const error = new Error({ message: "Not found" });
+      error.status = 404;
+      throw error;
     }
     res.json(result);
   } catch (error) {
