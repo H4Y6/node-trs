83136b07f667b8f95ded2fdc4623432b476dd9a9
H4Y6 <h4y6@i.ua>
2023-03-19 16:30:17 +0200

    new initiation

---
+++ node-rest-api-t/models/contacts.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const oneContact = contacts.find((c) => c.id === contactId);
+  if (!oneContact) {
+    return null;
+  }
+  return oneContact;
+};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ node-rest-api-t/routes/api/contacts.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const contacts = require("../../models/contacts");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
