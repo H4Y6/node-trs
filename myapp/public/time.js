c45f2be0aaeaa4ed43423cccc881653188de6457
H4Y6 <h4y6@i.ua>
2023-03-20 21:15:39 +0200

    modified:   myapp/public/time.js

--- myapp/public/time.js
+++ myapp/public/time.js
@@ -2,5 +2,5 @@ function getTime() {
   const time = new Date().toString().slice(0, 25);
   const timeRef = document.querySelector(".time");
   timeRef.innerHTML = time;
-  timeRef.style.cssText = " font-family: font-size:25px; text-align: center";
+  console.log(time, timeRef);
 }
