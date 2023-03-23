011c3edbe6c17a0be7a87e2350ea8ef908f2ebc5
H4Y6 <h4y6@i.ua>
2023-03-24 00:34:46 +0200

    modified:   5/mongodb-project/app.js renamed dir: 5/node-rest-api-previous -> 5/node-rest-api-previous-books/ new file:   5/node-rest-api-previous-books/MongodbPlayground-1.mongodb

--- 5/mongodb-project/app.js
+++ 5/mongodb-project/app.js
@@ -1,9 +1,20 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const { DB_HOST } = process.env;
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connect success"))
+//   .catch((error) => error.message);
+
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const { DB_HOST } = process.env;
 mongoose.set("strictQuery", false);
+const { DB_HOST } = process.env;
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database connect success"))
+  .then(() => {
+    console.log("Database`s connected");
+  })
   .catch((error) => error.message);
