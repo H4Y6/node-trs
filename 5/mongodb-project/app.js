2d811bf98c5d6d1692f7e92532c962feb921dcd6
H4Y6 <h4y6@i.ua>
2023-03-23 21:37:39 +0200

    init dir "6'

--- 5/mongodb-project/app.js
+++ 5/mongodb-project/app.js
@@ -1,21 +1,9 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const { DB_HOST } = process.env;
-// mongoose.set("strictQuery", false);
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connect success"))
-//   .catch((error) => error.message);
-
 const mongoose = require("mongoose");
+require("dotenv").config();
 
+const { DB_HOST } = process.env;
 mongoose.set("strictQuery", false);
-
-const DB_HOST =
-  "mongodb+srv://h4y6:KraD1LKSu9YqvCg0@cluster-46-1.v9jqvon.mongodb.net/books-db?retryWrites=true&w=majority";
-
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log("Database conect success"))
+  .then(() => console.log("Database connect success"))
   .catch((error) => error.message);
