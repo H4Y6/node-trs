05a7042a07e0387ec56a2ad88072bfb26adbc35c
H4Y6 <h4y6@i.ua>
2023-03-26 19:37:15 +0300

    new file:   6/node-rest-api-ctrl/models/books.js 	deleted:    6/node-rest-api-ctrl/models/books/books.json 	deleted:    6/node-rest-api-ctrl/models/books/index.js 	modified:   6/node-rest-api-ctrl/routes/api/books.js 	modified:   6/node-rest-api-ctrl/server.js
     Changes not staged for commit:
    	modified:   6/node-rest-api-ctrl/MongodbPlayground-1.mongodb
    	modified:   6/node-rest-api-ctrl/models/books.js
    	modified:   6/node-rest-api-ctrl/routes/api/books.js
    
     Untracked files:
    	6/node-rest-api-ctrl/controllers/

--- 6/node-rest-api-ctrl/server.js
+++ 6/node-rest-api-ctrl/server.js
@@ -1,30 +1,14 @@
-// const mongoose = require("mongoose");
-// mongoose.set("strictQuery", false);
-
-// const app = require("./app");
-
-// const { DB_HOST, PORT = 3000 } = process.env;
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => app.listen(PORT))
-//   .catch((error) => {
-//     console.log(error);
-//     process.exit(1);
-//   });
-
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const app = require("./app");
-const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.set("strictQuery", false);
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => app.listen(PORT))
   .catch((error) => {
     console.log(error);
-
     process.exit(1);
   });
