62067628ce9ab6483c06224299acdd57fead5dab
H4Y6 <h4y6@i.ua>
2023-04-03 19:20:27 +0300

    modified:   7/auth-example/controllers/auth/index.js modified:   7/auth-example/controllers/auth/login.js new file:   7/auth-example/controllers/auth/logout.js modified:   7/auth-example/controllers/books/getAll.js modified:   7/auth-example/middlewares/auth.js modified:   7/auth-example/models/user.js modified:   7/auth-example/routes/api/auth.js

--- 7/auth-example/controllers/auth/login.js
+++ 7/auth-example/controllers/auth/login.js
@@ -25,6 +25,7 @@ const login = async (req, res) => {
   }
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "22h" });
+  await User.findByIdAndUpdate(user._id, { token });
   res.json({ token });
 };
 
--- 7/auth-example/controllers/books/getAll.js
+++ 7/auth-example/controllers/books/getAll.js
@@ -4,10 +4,12 @@ const { Book } = require(`${basedir}/models/books`);
 
 const getAll = async (req, res, next) => {
   const { id: owner } = req.user;
-  const result = await Book.find({ owner }, "-createdAt -updatedAt").populate(
-    "owner",
-    "name email"
-  );
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Book.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "name email");
   // const result = await Book.find({}, "title author");
   res.json(result);
 };
