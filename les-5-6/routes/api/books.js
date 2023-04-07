3fb818f8440695c63e4bb0d9a56b3a313ffe33ce
H4Y6 <h4y6@i.ua>
2023-04-07 15:34:10 +0300

    init from les-4

---
+++ les-5-6/routes/api/books.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const Joi = require("joi");
+const books = require("../../models/books");
+const createError = require("../../helpers");
+const router = express.Router();
+
+const bookAddSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await books.getAll();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await books.getById(id);
+    if (!result) {
+      // return res.status(404).json({ message: "Not found" });
+      // const error = new Error("Not found");
+      // error.status = 404;
+      // throw error;
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    // res.status(500).json({ message: "Server eror" });
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = bookAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { title, author } = req.body;
+    const result = await books.add(title, author);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = bookAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { id } = req.params;
+    const { title, author } = req.body;
+    const result = await books.updateById(id, title, author);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await books.deleteById(id);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json({ message: "Book`s deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
