9e839e1a73d2ba200ce2aa5f772eca3a70b6081f
H4Y6 <h4y6@i.ua>
2023-04-08 13:01:27 +0300

    int new dir: less-5-6

---
+++ less-5-6/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+require("dotenv").config();
+
+const booksRouter = require("./routes/api/books");
+
+const app = express();
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatLogger));
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/books", booksRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
