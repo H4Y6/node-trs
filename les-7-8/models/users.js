f60d8b0cc24fb76555555a765105f0c2d6056e32
H4Y6 <h4y6@i.ua>
2023-04-11 14:51:53 +0300

    new file:   les-7-8/models/users.js

---
+++ les-7-8/models/users.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const emailRegexp = /\S+@\S+\.\w{2,3/;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, match: emailRegexp, unique: true, required: true },
+  password: { type: String, minlength: 6, required: true },
+});
+
+const User = model("user", userSchema);
+
+module.exports = { User };
