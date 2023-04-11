2860d4ba862b9e71b4a63070550423e376119829
H4Y6 <h4y6@i.ua>
2023-04-11 16:03:09 +0300

    new files: les-7-8/controllers/books/add.js, les-7-8/controllers/books/deleteById.js, les-7-8/controllers/books/updateById.js, les-7-8/controllers/books/updateStatus.js
    modified:   les-7-8/routes/api/books.js, les-7-8/controllers/books/index.js

---
+++ les-7-8/controllers/books/deleteById.js
@@ -0,0 +1,17 @@
+const { basedir } = global;
+const { Book } = require(`${basedir}/models/books`);
+const { createError } = require(`${basedir}/helpers`);
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Book.findByIdAndRemove(id);
+  if (!result) {
+    throw createError(404);
+  }
+  // res.status(204).send()
+  res.json({
+    message: "Book deleted",
+  });
+};
+
+module.exports = deleteById;
--- les-7-8/routes/api/books.js
+++ les-7-8/routes/api/books.js
@@ -1,94 +1,21 @@
 const express = require("express");
 
 const { basedir } = global;
+const { ctrlWrapper } = require(`${basedir}/helpers`);
 const ctrl = require(`${basedir}/controllers/books`);
-
-const Joi = require("joi");
-
-const Book = require("../../models/books");
-
-const { createError, ctrlWrapper } = require(`${basedir}/helpers`);
 
 const router = express.Router();
 
-const bookAddSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-  favorite: Joi.boolean(),
-  genre: Joi.string().valueOf("fancy", "love").required(),
-  isbn: Joi.string()
-    .pattern(/\d{3}-\d{3}-\d{4}-\d{2}-\d/)
-    .required(),
-});
-const updateStatusSchema = Joi.object({
-  favorite: Joi.boolean(),
-});
-
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:id", ctrlWrapper(ctrl));
+router.get("/:id", ctrlWrapper(ctrl.getById));
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = bookAddSchema.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const result = await Book.create(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", ctrlWrapper(ctrl.add));
 
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = bookAddSchema.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await Book.findByIdAndUpdate(id, req.body, { new: true });
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", ctrlWrapper(ctrl.updateById));
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Book.findByIdAndRemove(id);
-    if (!result) {
-      throw createError(404);
-    }
-    // res.status(204).send()
-    res.json({
-      message: "Book deleted",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:id", ctrlWrapper(ctrl.deleteById));
 
-router.patch("/:id/favorite", async (req, res, next) => {
-  try {
-    const { error } = updateStatusSchema.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await Book.findByIdAndUpdate(id, req.body, { new: true });
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch("/:id/favorite", ctrlWrapper(ctrl.updateStatus));
 
 module.exports = router;
