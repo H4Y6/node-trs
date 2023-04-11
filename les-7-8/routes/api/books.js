da8d6b7e4aa374c0cf5167f69ae72e9ff9cf854e
H4Y6 <h4y6@i.ua>
2023-04-11 15:43:40 +0300

    new file:   les-7-8/controllers/books/getById.js modified:   les-7-8/controllers/books/index.js modified:   les-7-8/routes/api/books.js

--- les-7-8/routes/api/books.js
+++ les-7-8/routes/api/books.js
@@ -26,19 +26,7 @@ const updateStatusSchema = Joi.object({
 
 router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Book.findById(id);
-    // const result = await Book.findOne({ _id: id });
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrlWrapper(ctrl));
 
 router.post("/", async (req, res, next) => {
   try {
