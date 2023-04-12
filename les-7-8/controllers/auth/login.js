99aa07d7d9497eb610ca26d2a40f46286aa87afc
H4Y6 <h4y6@i.ua>
2023-04-12 18:26:25 +0300

    new file:   controllers/auth/logout.js,
     modified:   app.js, controllers/auth/index.js, controllers/auth/login.js,
      controllers/books/getAll.js, middlewares/auth.js,
       models/users.js, routes/api/auth.js

--- les-7-8/controllers/auth/login.js
+++ les-7-8/controllers/auth/login.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
 
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "2d" });
-
+  await User.findByIdAndUpdate(user._id, { token });
   res.json(token);
 };
 
