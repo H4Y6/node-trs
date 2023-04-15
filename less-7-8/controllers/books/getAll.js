2ee5a6150ff9990b0bef02f0e2328f19a4180d74
H4Y6 <h4y6@i.ua>
2023-04-15 16:31:05 +0300

    modified:   less-7-8/controllers/books/getAll.js

--- less-7-8/controllers/books/getAll.js
+++ less-7-8/controllers/books/getAll.js
@@ -2,11 +2,13 @@ const { Book } = require("../../models/books");
 
 const getAll = async (req, res) => {
   const { id: owner } = req.user;
-  // const result = await Book.find({}, "-createdAt -updatedAt");
-  const result = await Book.find({ owner }, "title author").populate(
-    "owner",
-    "name email"
-  );
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Book.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "name email");
+
   res.json(result);
 };
 
