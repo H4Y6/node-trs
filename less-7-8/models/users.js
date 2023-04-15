7d7f3393ad92df5ef9af487b6a4284f31ca02e30
H4Y6 <h4y6@i.ua>
2023-04-15 15:33:44 +0300

    new file:   less-7-8/controllers/auth/getCurrent.js 	modified:   less-7-8/controllers/auth/index.js 	new file:   less-7-8/controllers/auth/logout.js 	modified:   less-7-8/controllers/books/add.js 	modified:   less-7-8/controllers/books/getAll.js 	new file:   less-7-8/middlewares/auth.js 	new file:   less-7-8/middlewares/index.js 	modified:   less-7-8/models/books.js 	modified:   less-7-8/models/users.js 	modified:   less-7-8/routes/api/auth.js 	modified:   less-7-8/routes/api/books.js

--- less-7-8/models/users.js
+++ less-7-8/models/users.js
@@ -8,6 +8,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, match: emailRegexp, unique: true, required: true },
     password: { type: String, minlength: 6, required: true },
+    token: { type: String },
   },
   { versionKey: false, timestamps: true }
 );
--- less-7-8/routes/api/auth.js
+++ less-7-8/routes/api/auth.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { basedir } = global;
 
 const { ctrlWrapper } = require("../../helpers");
+const { auth } = require(`${basedir}/middlewares`);
 
 const ctrl = require(`${basedir}/controllers/auth`);
 
@@ -10,5 +11,7 @@ const router = express.Router();
 
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
+router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 module.exports = router;
