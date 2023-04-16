77969da90eec55fdea2884883ccf9a6b702082ff
H4Y6 <h4y6@i.ua>
2023-04-16 19:03:31 +0300

    new file:   lssn-7-8/controllers/auth/getCurrent.js 	modified:   lssn-7-8/controllers/auth/index.js 	new file:   lssn-7-8/controllers/auth/logout.js 	modified:   lssn-7-8/controllers/books/add.js 	modified:   lssn-7-8/controllers/books/getAll.js 	new file:   lssn-7-8/middlewares/auth.js 	new file:   lssn-7-8/middlewares/index.js 	modified:   lssn-7-8/models/books.js 	modified:   lssn-7-8/routes/api/auth.js 	modified:   lssn-7-8/routes/api/books.js

--- lssn-7-8/controllers/books/getAll.js
+++ lssn-7-8/controllers/books/getAll.js
@@ -2,7 +2,13 @@ const { basedir } = global;
 const { Book } = require(`${basedir}/models/books`);
 
 const getAll = async (req, res, next) => {
-  const result = await Book.find({}, "-createdAt -updatedAt");
+  const { id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Book.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "name email");
   // const result = await Book.find({}, "title author");
   res.json(result);
 };
