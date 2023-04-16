6ef35e90f4d95caa6677ba3ffd7c7a288860b2d8
H4Y6 <h4y6@i.ua>
2023-04-16 23:30:35 +0300

    modified:   8/models/books.js

--- 8/models/books.js
+++ 8/models/books.js
@@ -1,19 +1,5 @@
 const Joi = require("joi");
 
-const addSchema = Joi.object({
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-  favorite: Joi.boolean(),
-  genres: Joi.string().valueOf("fancy", "science", "love").required(),
-  isbn: Joi.string()
-    .pattern(/\d{3}-\d{3}-\d{4}-\d{2}-\d/)
-    .required(),
-});
-
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean(),
-});
-
 const { Schema, model } = require("mongoose");
 
 const bookSchema = new Schema(
@@ -32,9 +18,24 @@ const bookSchema = new Schema(
       required: true,
       unique: true,
     },
+    owner: { type: Schema.Types.ObjectId, ref: "user" },
   },
   { versionKey: false, timestamps: true }
 );
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+  favorite: Joi.boolean(),
+  genres: Joi.string().valueOf("fancy", "science", "love").required(),
+  isbn: Joi.string()
+    .pattern(/\d{3}-\d{3}-\d{4}-\d{2}-\d/)
+    .required(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean(),
+});
 
 const schemas = {
   add: addSchema,
