860f90a90083641aef98bf8d922a84a4c3b80f08
H4Y6 <h4y6@i.ua>
2023-04-17 00:18:09 +0300

    modified:   8/controllers/auth/login.js, 8/controllers/auth/logout.js

--- 8/controllers/auth/logout.js
+++ 8/controllers/auth/logout.js
@@ -4,6 +4,7 @@ const { User } = require(`${basedir}/models/users`);
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).send();
 };
 
 module.exports = logout;
