0f8e7b956f5d5b1b13b10c190af178efe416ae50
H4Y6 <h4y6@i.ua>
2023-04-16 11:51:53 +0300

    modified:   lssn-7-8/app.js 	new file:   lssn-7-8/controllers/books/add.js 	new file:   lssn-7-8/controllers/books/getAll.js 	new file:   lssn-7-8/controllers/books/index.js 	modified:   lssn-7-8/routes/api/books.js

--- lssn-7-8/routes/api/books.js
+++ lssn-7-8/routes/api/books.js
@@ -44,18 +44,7 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = bookAddSchema.validate(req.body);
-    if (error) {
-      throw createError(400, error.message);
-    }
-    const result = await Book.create(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/");
 
 router.put("/:id", async (req, res, next) => {
   try {
