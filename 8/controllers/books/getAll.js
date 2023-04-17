e1b3d82b5f1237a3fe6eb8cb89750280e8d7c1f4
H4Y6 <h4y6@i.ua>
2023-04-17 13:53:50 +0300

    modified:   8/controllers/books/getAll.js

--- 8/controllers/books/getAll.js
+++ 8/controllers/books/getAll.js
@@ -2,15 +2,14 @@ const { basedir } = global;
 
 const { Book } = require(`${basedir}/models/books`);
 
-const getAll = async (req, res, next) => {
-  const { id: owner } = req.user;
+const getAll = async (req, res) => {
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
+  const { id: owner } = req.user;
   const result = await Book.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit: Number(limit),
   }).populate("owner", "name email");
-  // const result = await Book.find({}, "title author");
   res.json(result);
 };
 
