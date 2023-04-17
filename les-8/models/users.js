f41f026c192e04bb7c27aacfa14046b06acc4beb
H4Y6 <h4y6@i.ua>
2023-04-17 16:56:49 +0300

    new files:   les-8/controllers/auth/index.js, les-8/controllers/auth/register.js 	modified:   les-8/app.js,  les-8/models/users.js

--- les-8/models/users.js
+++ les-8/models/users.js
@@ -1,17 +1,12 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
-const emailRegexp = /^\S+@\S+\.\w{2,3$/;
+const emailRegexp = /^\S+@\S+\.\w{2,3}$/;
 
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: {
-      type: String,
-      match: emailRegexp,
-      required: true,
-      unique: true,
-    },
+    email: { type: String, match: emailRegexp, unique: true, required: true },
     password: { type: String, minlength: 6, required: true },
   },
   { versionKey: false, timestamps: true }
@@ -22,12 +17,13 @@ const registerSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
-const loginStatusSchema = Joi.object({
+
+const loginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
 
-const schemas = { register: registerSchema, login: loginStatusSchema };
+const schemas = { register: registerSchema, login: loginSchema };
 
 const User = model("user", userSchema);
 
