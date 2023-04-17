1fc7c8a2561378d461a9c59222111f8217346b8a
H4Y6 <h4y6@i.ua>
2023-04-17 21:44:57 +0300

    new file:   les-8/controllers/auth/getCurrent.js 	modified:   les-8/controllers/auth/index.js 	modified:   les-8/routes/api/auth.js

--- les-8/routes/api/auth.js
+++ les-8/routes/api/auth.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const { basedir } = global;
 const ctrl = require(`${basedir}/controllers/auth`);
 const { ctrlWrapper } = require(`${basedir}/helpers`);
+const { auth } = require(`${basedir}/middlewares`);
 
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
+router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
 module.exports = router;
