758513aa134257571668160bad7d144e46ed1704
H4Y6 <h4y6@i.ua>
2023-04-18 16:35:17 +0300

    new file:   less-8/controllers/auth/logout.js 	modified:   less-8/controllers/books/add.js 	modified:   less-8/controllers/books/getAll.js 	modified:   less-8/controllers/books/updateById.js 	modified:   less-8/models/books.js 	modified:   less-8/routes/api/auth.js 	modified:   less-8/routes/api/books.js 	modified:   less-8/controllers/auth/index.js

--- less-8/controllers/books/add.js
+++ less-8/controllers/books/add.js
@@ -1,13 +1,14 @@
 const { Book, schemas } = require("../../models/books");
 
-const createError = require("../../helpers");
+const { createError } = require("../../helpers");
 
 const add = async (req, res) => {
   const { error } = schemas.add.validate(req.body);
   if (error) {
     throw createError(400, error.message);
   }
-  const result = await Book.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Book.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- less-8/models/books.js
+++ less-8/models/books.js
@@ -1,10 +1,33 @@
 const Joi = require("joi");
 
+const { Schema, model } = require("mongoose");
+
+const bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genres: {
+      type: String,
+      enum: ["fancy", "science", "love"],
+      required: true,
+    },
+    favorite: { type: Boolean, default: false },
+    isbn: {
+      type: String,
+      match: /\d{3}-\d{3}-\d{4}-\d{2}-\d/,
+      required: true,
+      unique: true,
+    },
+    owner: { type: Schema.Types.ObjectId, ref: "user" },
+  },
+  { versionKey: false, timestamps: true }
+);
+
 const addSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
   favorite: Joi.boolean(),
-  genre: Joi.string().valueOf("fancy", "love").required(),
+  genres: Joi.string().valueOf("fancy", "science", "love").required(),
   isbn: Joi.string()
     .pattern(/\d{3}-\d{3}-\d{4}-\d{2}-\d/)
     .required(),
@@ -13,24 +36,6 @@ const addSchema = Joi.object({
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean(),
 });
-
-const { Schema, model } = require("mongoose");
-
-const bookSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    genre: { type: String, enum: ["fancy", "love"], required: true },
-    favorite: { type: Boolean, default: false },
-    isbn: {
-      type: String,
-      match: /\d{3}-\d{3}-\d{4}-\d{2}-\d/,
-      required: true,
-      unique: true,
-    },
-  },
-  { versionKey: false, timestamps: true }
-);
 
 const schemas = {
   add: addSchema,
