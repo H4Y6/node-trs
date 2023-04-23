2ab078b58f04ab617a0272a7d83e4c3d482498ea
H4Y6 <h4y6@i.ua>
2023-04-23 19:29:20 +0300

    modified:   9/multer-example/app.js

--- 9/multer-example/app-1.js
+++ 9/multer-example/app-1.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs/promises");
-const {nanoid} = require("nanoid");
+const { nanoid } = require("nanoid");
 
 const app = express();
 
@@ -14,42 +14,93 @@ app.use(express.static("public"));
 const tempDir = path.join(__dirname, "temp");
 
 const multerConfig = multer.diskStorage({
-    destination: tempDir,
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({
-    storage: multerConfig
-})
+  storage: multerConfig,
+});
 
 const books = [];
 
 // upload.fields([{name: "cover", maxCount: 2}, {name: "side-cover", maxCount: 2}])
 // upload.array("cover", 8)
 
-app.get("/api/books", (req, res)=> {
-    res.json(books);
-})
+app.get("/api/books", (req, res) => {
+  res.json(books);
+});
 
 const booksDir = path.join(__dirname, "public", "books");
-app.post("/api/books", upload.single("cover"), async (req, res)=> {
-    try {
-        const {path: tempPath, originalname} = req.file;
-        const uploadPath = path.join(booksDir, originalname);
-        await fs.rename(tempPath, uploadPath);
-        const cover = path.join("books", originalname)
-        const newBook = {
-            name: req.body.name,
-            cover,
-            id: nanoid()
-        };
-        books.push(newBook);
-        res.status(201).json(newBook);
-    } catch (error) {
-        await fs.unlink(req.file.path);
-    }
-})
+app.post("/api/books", upload.single("cover"), async (req, res) => {
+  try {
+    const { path: tempPath, originalname } = req.file;
+    const uploadPath = path.join(booksDir, originalname);
+    await fs.rename(tempPath, uploadPath);
+    const cover = path.join("books", originalname);
+    const newBook = {
+      name: req.body.name,
+      cover,
+      id: nanoid(),
+    };
+    books.push(newBook);
+    res.status(201).json(newBook);
+  } catch (error) {
+    await fs.unlink(req.file.path);
+  }
+});
 
 app.listen(3000);
+
+// const express = require("express");
+// const cors = require("cors");
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs/promises");
+// const { nanoid } = require("nanoid");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// const tempDir = path.join(__dirname, "temp");
+
+// const multerConfig = multer.diskStorage({
+//   destination: tempDir,
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: multerConfig });
+
+// const books = [];
+
+// app.get("/api/books", (req, res) => {
+//   res.json(books);
+// });
+
+// const booksDir = path.join(__dirname, "public", "books");
+// app.post("/api/books", upload.single("cover"), async (req, res) => {
+//   try {
+//     const { path: tempPath, originalname } = req.file;
+//     const uploadPath = path.join(booksDir, originalname);
+//     await fs.rename(tempPath, uploadPath);
+//     const cover = path.join("books", originalname);
+//     const newBook = {
+//       name: req.body.name,
+//       cover,
+//       id: nanoid(),
+//     };
+//     books.push(newBook);
+//     res.status(201).json(newBook);
+//   } catch (error) {
+//     // await fs.unlink(req.file.path);
+//   }
+// });
+
+// app.listen(3000, () => console.log("Listen to port: 3000"));
