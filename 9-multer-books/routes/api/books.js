43fed35decdca924523fecccbdcad0be98038fe4
h4y6 <h4y6@i.ua>
2023-04-27 17:19:12 +0300

    new dir init:   9-multer-books

---
+++ 9-multer-books/routes/api/books.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const { basedir } = global;
+
+const ctrl = require(`${basedir}/controllers/books`);
+const { ctrlWrapper } = require(`${basedir}/helpers`);
+const { auth } = require(`${basedir}/middlewares`);
+
+const router = express.Router();
+
+router.get("/", auth, ctrlWrapper(ctrl.getAll));
+
+router.get("/:id", auth, ctrlWrapper(ctrl.getById));
+
+router.post("/", auth, ctrlWrapper(ctrl.add));
+
+router.put("/:id", auth, ctrlWrapper(ctrl.updateById));
+
+router.delete("/:id", auth, ctrlWrapper(ctrl.removeById));
+
+router.patch("/:id/favorite", auth, ctrlWrapper(ctrl.updateFavorite));
+
+module.exports = router;
