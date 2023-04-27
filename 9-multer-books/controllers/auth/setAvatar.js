d41192fc6548adf06b2862ba40f30503068e10ea
h4y6 <h4y6@i.ua>
2023-04-27 18:31:51 +0300

    new file:   9-multer-books/controllers/auth/setAvatar.js 	new file:   9-multer-books/public/avatars/644a88625c708259267df8a2.jpg

---
+++ 9-multer-books/controllers/auth/setAvatar.js
@@ -0,0 +1,26 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const { basedir } = global;
+const { User } = require(`${basedir}/models/users`);
+
+const avatarsDir = path.join(basedir, "public", "avatars");
+
+const setAvatar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { path: tempPath, originalname } = req.file;
+    const [extension] = originalname.split(".").reverse();
+    const newName = `${_id}.${extension}`;
+    const uploadPath = path.join(avatarsDir, newName);
+    await fs.rename(tempPath, uploadPath);
+    const avatarURL = path.join("avatars", newName);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({ avatarURL });
+  } catch (error) {
+    await fs.unlink(req.file.path);
+    throw error;
+  }
+};
+
+module.exports = setAvatar;
