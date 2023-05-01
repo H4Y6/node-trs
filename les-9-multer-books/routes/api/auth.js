c1637480cb90d69ef34d05af2deb64047e144a5f
h4y6 <h4y6@i.ua>
2023-05-01 23:22:37 +0300

    modified:   les-9-multer-books/controllers/auth/index.js 	modified:   les-9-multer-books/controllers/auth/setAvatar.js 	modified:   les-9-multer-books/routes/api/auth.js

--- les-9-multer-books/routes/api/auth.js
+++ les-9-multer-books/routes/api/auth.js
@@ -11,7 +11,7 @@ router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
-router.paatch(
+router.patch(
   "/avatars",
   auth,
   upload.single("avatar"),
