be4762a347e19f7986d4e3a376c6d9060264790f
h4y6 <h4y6@i.ua>
2023-05-01 22:45:28 +0300

    modified:   les-9-multer-books/middlewares/index.js 	new file:   les-9-multer-books/middlewares/upload.js 	modified:   les-9-multer-books/routes/api/auth.js

--- les-9-multer-books/routes/api/auth.js
+++ les-9-multer-books/routes/api/auth.js
@@ -5,12 +5,17 @@ const router = express.Router();
 const { basedir } = global;
 const ctrl = require(`${basedir}/controllers/auth`);
 const { ctrlWrapper } = require(`${basedir}/helpers`);
-const { auth } = require(`${basedir}/middlewares`);
+const { auth, upload } = require(`${basedir}/middlewares`);
 
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 router.get("/logout", auth, ctrlWrapper(ctrl.logout));
-router.paatch("/avatars", auth, ctrlWrapper(ctrl.setAvatar));
+router.paatch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.setAvatar)
+);
 
 module.exports = router;
