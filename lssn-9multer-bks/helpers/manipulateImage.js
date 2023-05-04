93432d2202ec4ba884b71044ca1f6d9e0cf295c2
h4y6 <h4y6@i.ua>
2023-05-04 12:44:01 +0300

    modified:   lssn-9multer-bks/controllers/auth/setAvatar.js 	modified:   lssn-9multer-bks/helpers/manipulateImage.js 	renamed:    lssn-9multer-bks/img.jpg -> lssn-9multer-bks/temp/img.jpg

--- lssn-9multer-bks/helpers/manipulateImage.js
+++ lssn-9multer-bks/helpers/manipulateImage.js
@@ -2,7 +2,7 @@ const Jimp = require("jimp");
 
 const manipulateImage = (tempPath) =>
   Jimp.read(tempPath).then((image) => {
-    return image.resize(250, 250).quality(79).write("img.jpg");
+    return image.resize(250, 250).quality(79).write("temp/img.jpg");
   });
 
 module.exports = manipulateImage;
