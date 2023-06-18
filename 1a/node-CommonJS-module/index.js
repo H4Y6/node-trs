a30642c1126ed25fdef60864aa17d117b4619dcd
Heorhii Chub <h4y6@i.ua>
2023-06-18 19:49:25 +0300

    modified:   1a/node-CommonJS-module/index.js

--- 1a/node-CommonJS-module/index.js
+++ 1a/node-CommonJS-module/index.js
@@ -4,8 +4,9 @@ const admins = users.admins;
 // const { admins } = require("./users");
 
 // const thisMonth = require("./date").getCurrentMonth();
-const currentMonth = require("./date").getCurrentMonth();
+const { getCurrentMonth } = require("./date");
+// const currentMonth = require("./date").getCurrentMonth();
 
 console.log(users);
 console.log(admins);
-console.log(currentMonth);
+console.log(getCurrentMonth());
