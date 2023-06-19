fcf36a2bf0ad442810587f6fc954b2ee72feacae
Heorhii Chub <h4y6@i.ua>
2023-06-19 13:04:42 +0300

    modified:   1a/work-with-files/app.js

--- 1a/work-with-files/app.js
+++ 1a/work-with-files/app.js
@@ -1,6 +1,14 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.readFile("./files/file.txt", (error, data) => {
-  console.log(error);
-  console.log(data.toString());
-});
+// fs.readFile("./files/file.txt", (error, data) => {
+//   console.log(error);
+//   console.log(data);
+// });
+
+const { error } = require("console");
+const fs = require("fs/promises");
+// const fs = require("fs").promises;
+
+fs.readFile("./files/file.txt")
+  .then((data) => console.log(data.toString()))
+  .catch((error) => console.log(error));
