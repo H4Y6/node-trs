8e1a54a2c8ccb876fa2266ccb301fe8528a3ff8e
Heorhii Chub <h4y6@i.ua>
2023-06-19 12:54:05 +0300

    modified:   1a/work-with-files/app.js

--- 1a/work-with-files/app.js
+++ 1a/work-with-files/app.js
@@ -3,5 +3,6 @@ const fs = require("fs");
 
 fs.readFile("./files/file.txt", (error, data) => {
   console.log(error);
-  console.log(data);
+  // console.log(data);
+  console.log(data.toString());
 });
