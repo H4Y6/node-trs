a98d0702bb0ac56fd3dedbef29b68b6d63b3d560
Heorhii Chub <h4y6@i.ua>
2023-06-19 13:26:29 +0300

    modified:   1a/work-with-files/app.js

--- 1a/work-with-files/app.js
+++ 1a/work-with-files/app.js
@@ -20,8 +20,8 @@ const fs = require("fs/promises");
 const fileOperation = async ({ filePath, action, data }) => {
   switch (action) {
     case "read":
-      const data = await fs.readFile(filePath, "utf-8");
-      console.log(data);
+      const text = await fs.readFile(filePath, "utf-8");
+      console.log(text);
       break;
 
     default:
