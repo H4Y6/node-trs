0d5997de497aadb4f03ae32db6e14e6f51f99469
Heorhii Chub <h4y6@i.ua>
2023-06-19 13:36:42 +0300

    modified:   1a/work-with-files/app.js

--- 1a/work-with-files/app.js
+++ 1a/work-with-files/app.js
@@ -23,10 +23,26 @@ const fileOperation = async ({ filePath, action, data }) => {
       const text = await fs.readFile(filePath, "utf-8");
       console.log(text);
       break;
+    case "add":
+      await fs.appendFile(filePath, data);
+      break;
+    case "replace":
+      await fs.writeFile(filePath, data);
+      break;
 
     default:
       break;
   }
 };
 
-fileOperation({ filePath: "./files/file.txt", action: "read" });
+// fileOperation({ filePath: "./files/file.txt", action: "read" });
+// fileOperation({
+//   filePath: "./files/file.txt",
+//   action: "add",
+//   data: "\nHello!",
+// });
+fileOperation({
+  filePath: "./files/file.txt",
+  action: "replace",
+  data: "Good day!",
+});
