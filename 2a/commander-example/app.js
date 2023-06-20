7fce95124d1e4efd0147fb746133ed83554addcb
Heorhii Chub <h4y6@i.ua>
2023-06-20 19:19:22 +0300

    init 2a/commander-example

---
+++ 2a/commander-example/app.js
@@ -0,0 +1,42 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const books = require("./books");
+
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case "getAll":
+      const allBooks = await books.getAll();
+      console.log(allBooks);
+      break;
+    case "getById":
+      const oneBook = await books.getById(id);
+      console.log(oneBook);
+      break;
+    case "add":
+      const newBook = await books.add(title, author);
+      console.log(newBook);
+      break;
+    case "updateById":
+      const updateBook = await books.updateById(id, title, author);
+      console.log(updateBook);
+      break;
+    case "deleteById":
+      const deleBook = await books.deleteById(id);
+      console.log(deleBook);
+      break;
+  }
+};
+
+// const arr = hideBin(process.argv);
+// const arr2 = process.argv.slice(2);
+// console.log(arr);
+// console.log(arr2);
+
+// const { argv } = yargs(arr);
+// const { argv } = yargs(arr2);
+// console.log(argv);
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
---
+++ 2a/commander-example/books/index.js
@@ -0,0 +1,54 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const booksPath = path.join(__dirname, "books.json");
+
+const updateBooks = async (books) =>
+  await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+
+const getAll = async () => {
+  const data = await fs.readFile(booksPath);
+  return JSON.parse(data);
+};
+
+const getById = async (id) => {
+  const books = await getAll();
+  const result = books.find((b) => b.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const add = async (title, author) => {
+  const books = await getAll();
+  const newBook = { id: nanoid(), title, author };
+  books.push(newBook);
+  await updateBooks(books);
+  return newBook;
+};
+
+const updateById = async (id, title, author) => {
+  const books = await getAll();
+  const idx = books.findIndex((b) => b.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  books[idx] = { id, title, author };
+  await updateBooks(books);
+  return books[idx];
+};
+
+const deleteById = async (id) => {
+  const books = await getAll();
+  const i = books.findIndex((b) => b.id === id);
+  if (i === -1) {
+    return null;
+  }
+  const [deleBook] = books.splice(i, 1);
+  await updateBooks(books);
+  return deleBook;
+};
+
+module.exports = { getAll, getById, add, updateById, deleteById };
