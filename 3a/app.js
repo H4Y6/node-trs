2cf5544a30cfcceae8e1ea7d9463fad7ae32c0e2
Heorhii Chub <h4y6@i.ua>
2023-06-22 15:55:46 +0300

    modified:   3a/app.js

--- 3a/app.js
+++ 3a/app.js
@@ -3,3 +3,15 @@ const express = require("express");
 const app = express(); //app = web-server
 
 app.listen(3000, () => console.log("Server is running"));
+
+app.get("/", (req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+  res.send("<h2>Home page</h2>");
+});
+
+app.get("/contacts", (req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+  res.send("<h2>Contact page</h2>");
+});
