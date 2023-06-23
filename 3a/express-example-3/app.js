603ef0c965728636a0d8c69e8973d705cd1c60b1
Heorhii Chub <h4y6@i.ua>
2023-06-23 12:54:47 +0300

    modified:   3a/express-example-3/app.js

--- 3a/express-example-3/app.js
+++ 3a/express-example-3/app.js
@@ -1,39 +1,47 @@
+// const express = require("express");
+// const fs = require("fs/promises");
+// const cors = require("cors");
+
+// const books = require("./books");
+
+// const app = express();
+
+// app.use(cors());
+
+// app.use(async (req, res, next) => {
+//   const { method, url } = req;
+//   const currentTime = new Date();
+//   await fs.appendFile(
+//     "./server.log",
+//     `${currentTime.toString().slice(0, 25)} ${method} ${url}\n`
+//   );
+//   next();
+// });
+
+// // app.use((req, res, next) => {
+// //   console.log("first middleware");
+// //   next();
+// // });
+
+// // app.use((req, res, next) => {
+// //   console.log("second middleware");
+// //   next();
+// // });
+
+// app.get("/books", (req, res) => {
+//   res.json(books);
+// });
+
+// app.get("/products", (req, res) => {
+//   res.json([]);
+// });
+
+// app.listen(3000);
+
 const express = require("express");
-const fs = require("fs/promises");
-const cors = require("cors");
 
 const books = require("./books");
 
 const app = express();
-
-app.use(cors());
-
-app.use(async (req, res, next) => {
-  const { method, url } = req;
-  const currentTime = new Date();
-  await fs.appendFile(
-    "./server.log",
-    `${currentTime.toString().slice(0, 25)} ${method} ${url}\n`
-  );
-  next();
-});
-
-// app.use((req, res, next) => {
-//   console.log("first middleware");
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log("second middleware");
-//   next();
-// });
-
-app.get("/books", (req, res) => {
-  res.json(books);
-});
-
-app.get("/products", (req, res) => {
-  res.json([]);
-});
 
 app.listen(3000);
