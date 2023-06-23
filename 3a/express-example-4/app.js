9b40ec04538690aac549521ce4931ce2f2bb40de
Heorhii Chub <h4y6@i.ua>
2023-06-23 15:26:08 +0300

    modified:   3a/express-example-4/app.js 	modified:   3a/express-example-4/routes/api/books.js

--- 3a/express-example-4/app.js
+++ 3a/express-example-4/app.js
@@ -22,4 +22,6 @@ const app = express();
 app.use(cors());
 app.use("/api/books", booksRouter);
 
+app.use((req, res) => [res.status(404).json({ message: "Not found" })]);
+
 app.listen(3000);
--- 3a/express-example-4/routes/api/books.js
+++ 3a/express-example-4/routes/api/books.js
@@ -25,5 +25,6 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {});
 router.post("/", (req, res) => {});
 router.put("/", (req, res) => {});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
