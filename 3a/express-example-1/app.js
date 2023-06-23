7a647e3a055420d00d5783bf36272da8be608ce6
Heorhii Chub <h4y6@i.ua>
2023-06-23 12:27:37 +0300

    modified:   3a/express-example-1/app.js

--- 3a/express-example-1/app.js
+++ 3a/express-example-1/app.js
@@ -20,6 +20,17 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  const { method, url } = req;
+
+  res.send("<h2>Home page</h2>");
+});
+
+app.get("/contacts", (req, res) => {
+  console.log(req.method, req.url);
+  res.send("<h2>Contact page</h2>");
+});
+
 app.listen(3000, () => {
   console.log("Server`s running.");
 });
