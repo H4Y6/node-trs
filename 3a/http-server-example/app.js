9f912d3f3b0f1d0035135d98a847dea3f22b533e
Heorhii Chub <h4y6@i.ua>
2023-06-23 11:53:23 +0300

    modified:   3a/http-server-example/app.js

--- 3a/http-server-example/app.js
+++ 3a/http-server-example/app.js
@@ -1,28 +1,28 @@
 const http = require("http");
-// request - объект, описывающий весь запрос
-// response - объект, описывающий весь ответ
+// request - object with all request data
+// response - object with all responce data
 
-const server = http.createServer((request, response)=>{
-    const {url} = request;
-    if(url === "/"){
-        response.write("<h2>Home page</h2>");
-    } else if(url === "/contacts") {
-        response.write("<h2>Contacts page</h2>");
-    } else {
-        response.write("<h2>Not found</h2>");
-    }
-    response.end();
+const server = http.createServer((request, response) => {
+  const { url } = request;
+  if (url === "/") {
+    response.write("<h2>Home page</h2>");
+  } else if (url === "/contacts") {
+    response.write("<h2>Contacts page</h2>");
+  } else {
+    response.write("<h2>Not found</h2>");
+  }
+  response.end();
 });
 
-// const server = http.createServer((request, response)=>{
-//     // console.log(request.url);
-//     // console.log(request.headers["user-agent"]);
-//     // console.log(request.method);
+// const server = http.createServer((request, response) => {
+//   console.log(request.url);
+//   console.log(request.headers["user-agent"]);
+//   console.log(request.method);
 
-//     response.write("<h2>Добро пожаловать на наш сайт!</h2>");
-//     response.end();
+//   response.write("<h2>Welcome!</h2>");
+//   response.end();
 // });
 
-server.listen(3000, ()=> {
-    console.log("Сервер запущен!");
+server.listen(3000, () => {
+  console.log("Server is running");
 });
