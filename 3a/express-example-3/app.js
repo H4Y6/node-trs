7fec1e96cdfee35a9db9566c04ef76293023373e
Heorhii Chub <h4y6@i.ua>
2023-06-23 14:15:39 +0300

    modified:   3a/express-example-3/app.js

--- 3a/express-example-3/app.js
+++ 3a/express-example-3/app.js
@@ -39,10 +39,32 @@
 // app.listen(3000);
 
 const express = require("express");
+const cors = require("cors");
+const fs = require("fs/promises");
+const moment = require("moment");
 
 const books = require("./books");
 
 const app = express();
+app.use(cors());
+
+app.use(async (req, res, next) => {
+  const currentTime = new Date().toLocaleString();
+  // const currentTime = moment().format("DD.MM.YYYY hh:mm:ss");
+  const { method, url } = req;
+  await fs.appendFile("server.log", `${currentTime}: ${method} ${url}\n`);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   console.log("first middleware");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("second middleware");
+//   next();
+// });
 
 app.get("/books", (req, res) => {
   res.json(books);
