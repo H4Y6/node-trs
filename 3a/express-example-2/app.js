712a7d9a80dc6c513013390423fe75fadb1c6aef
Heorhii Chub <h4y6@i.ua>
2023-06-23 12:49:58 +0300

    modified:   3a/express-example-2/app.js

--- 3a/express-example-2/app.js
+++ 3a/express-example-2/app.js
@@ -1,13 +1,30 @@
+// const express = require("express");
+// const books = require("./books");
+
+// const app = express();
+
+// app.set("json spacees", 8);
+
+// app.get("/books", (req, res) => {
+//   res.json(books);
+//   // res.send(books);
+// });
+
+// app.listen(3000);
+
 const express = require("express");
+
 const books = require("./books");
 
 const app = express();
 
-app.set("json spacees", 8);
+// app.set("json spaces", 12);
 
 app.get("/books", (req, res) => {
-  res.json(books);
   // res.send(books);
+  // res.send(null);
+  // res.json(null);
+  res.json(books);
 });
 
 app.listen(3000);
