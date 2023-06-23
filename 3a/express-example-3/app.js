44599dd1fb643af76dfe609aae778030395990c3
Heorhii Chub <h4y6@i.ua>
2023-06-23 12:57:42 +0300

    modified:   3a/express-example-3/app.js

--- 3a/express-example-3/app.js
+++ 3a/express-example-3/app.js
@@ -44,4 +44,12 @@ const books = require("./books");
 
 const app = express();
 
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.get("/products", (req, res) => {
+  res.json([]);
+});
+
 app.listen(3000);
