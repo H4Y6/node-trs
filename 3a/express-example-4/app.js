8625b4bc1192977049a583768e2997cf80dab813
Heorhii Chub <h4y6@i.ua>
2023-06-23 15:32:18 +0300

    modified:   3a/express-example-4/app.js 	new file:   3a/express-example-4/routes/api/contacts.js

--- 3a/express-example-4/app.js
+++ 3a/express-example-4/app.js
@@ -17,10 +17,12 @@
 const express = require("express");
 const cors = require("cors");
 const booksRouter = require("./routes/api/books");
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 app.use(cors());
 app.use("/api/books", booksRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => [res.status(404).json({ message: "Not found" })]);
 
