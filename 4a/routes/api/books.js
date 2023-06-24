97b2110710f12eeeac2feb5ff0cb7c78e6a02049
Heorhii Chub <h4y6@i.ua>
2023-06-24 17:31:18 +0300

    modified:   4a/routes/api/books.js

--- 4a/routes/api/books.js
+++ 4a/routes/api/books.js
@@ -1,5 +1,12 @@
 const express = require("express");
 
+const books = require("../../models/books");
+
 const routes = express.Router();
+
+routes.get("/", async (req, res) => {
+  const result = await books.getAll();
+  res.json(result);
+});
 
 module.exports = routes;
