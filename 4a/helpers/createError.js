d519c58df32b2286e187fe88e9a1ee05ea6fe2ad
Heorhii Chub <h4y6@i.ua>
2023-06-25 15:57:17 +0300

    modified:   4a/app.js 	modified:   4a/helpers/createError.js 	modified:   4a/models/books/books.json 	modified:   4a/package.json 	modified:   4a/routes/api/books.js

--- 4a/helpers/createError.js
+++ 4a/helpers/createError.js
@@ -1,11 +1,10 @@
 const messages = {
-  400: "Bad request",
+  400: "Bad Request",
   401: "Unauthorized",
   403: "Forbidden",
   404: "Not found",
   409: "Conflict",
 };
-
 const createError = (status, message = messages[status]) => {
   const error = new Error(message);
   error.status = status;
--- 4a/routes/api/books.js
+++ 4a/routes/api/books.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const Joi = require("joi");
 
 const { createError } = require("../../helpers");
 
 const books = require("../../models/books");
 
 const router = express.Router();
+
+const bookAddSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -35,6 +41,10 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    const { error } = bookAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
     const result = await books.add(req.body);
     res.status(201).json(result);
   } catch (error) {
