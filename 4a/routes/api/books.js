252c23b44eeaf8948a8171450dd117bc72a9530a
Heorhii Chub <h4y6@i.ua>
2023-06-25 16:09:08 +0300

    modified:   4a/routes/api/books.js

--- 4a/routes/api/books.js
+++ 4a/routes/api/books.js
@@ -52,4 +52,21 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = bookAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await books.updateById(id, req.body);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
