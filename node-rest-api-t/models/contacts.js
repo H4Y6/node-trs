1f93313086d94027d3cca5e294fdc40af7c9ce1d
Heorhii Chub <h4y6@i.ua>
2023-06-26 15:23:22 +0300

    new file:   node-rest-api-t/package-lock.json

---
+++ node-rest-api-t/models/contacts.js
@@ -0,0 +1,12 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {};
+
+const getContactById = async (contactId) => {};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (name, email, phone) => {};
