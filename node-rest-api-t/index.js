1a7f796cca34b40641f857f872c84089f79f230d
Heorhii Chub <h4y6@i.ua>
2023-06-26 19:50:33 +0300

    modified:   node-rest-api-t/index.js

--- node-rest-api-t/index.js
+++ node-rest-api-t/index.js
@@ -1,6 +1,19 @@
 const contacts = require("./contacts");
 
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
