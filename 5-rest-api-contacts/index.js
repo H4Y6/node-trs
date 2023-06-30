8761a717d8d887e1027be6880205b52f0ee3ff06
Heorhii Chub <h4y6@i.ua>
2023-06-30 14:24:42 +0300

    modified:   5-rest-api-contacts/index.js

--- 5-rest-api-contacts/index.js
+++ 5-rest-api-contacts/index.js
@@ -1,4 +1,17 @@
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const contacts = require("./contacts");
 
