c1d49da18c6882d78c2e3abcbbd94289b9a34870
Heorhii Chub <h4y6@i.ua>
2023-06-30 12:52:10 +0300

    modified:   5-rest-api-contacts/contacts.js 	modified:   5-rest-api-contacts/index.js

--- 5-rest-api-contacts/index.js
+++ 5-rest-api-contacts/index.js
@@ -8,6 +8,10 @@ async function invokeAction({ action, id, name, email, phone }) {
       const allContacts = await contacts.listContacts();
       console.log(allContacts);
       break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
