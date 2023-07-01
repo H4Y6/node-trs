61715314764d1027210ded25843cbcf59aec68f2
Heorhii Chub <h4y6@i.ua>
2023-07-01 16:45:15 +0300

    new dir:   4b modified: 5-rest-api-contacts/app.js, 5/mongodb-project/package-lock.json,

--- 4b/app.js
+++ 4b/app.js
@@ -1,24 +1,28 @@
 const express = require("express");
-const logger = require("morgan");
 const cors = require("cors");
-global.basedir = __dirname;
-
-const contactsRouter = require("./routes/api/contacts");
+const logger = require("morgan");
+const booksRouter = require("./routes/api/books");
 
 const app = express();
-
 const formatLogger = app.get("env") === "development" ? "dev" : "short";
+
 app.use(logger(formatLogger));
+
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/contacts", contactsRouter);
+app.use("/api/books", booksRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
+  // app.use((err, _, res, __) => {
+  // const { status = 500, message = "Server error" } = err;
+  // res.status(status).json({ message });
+
+  // res.status(500).json({ message: err.message });
+
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
