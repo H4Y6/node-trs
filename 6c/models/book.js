9d99af46477baa083addb4912e9edbfd2b8a8630
Heorhii Chub <h4y6@i.ua>
2023-07-03 13:47:42 +0300

    new files:   6c/helpers/createError.js, 6c/helpers/index.js 	modified: 6c/app.js,  6c/models/book.js, modified:   6c/routes/api/books.js

--- 6c/models/book.js
+++ 6c/models/book.js
@@ -1,6 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-const bookSchema = new Schema({ title: String, author: String });
+const bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: ["fantastic", "love", "science"],
+      isbn: {
+        type: String,
+        match: /[0-9]{3}-[0-9]{1}-[0-9]{3}-[0-9]{5}-[0-9]/,
+      },
+      required: true,
+    },
+    favorite: { type: Boolean, default: false },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const Book = model("book", bookSchema);
 
