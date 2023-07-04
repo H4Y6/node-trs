30caf8085189405e4ec755be6fc41b4c1843091f
Heorhii Chub <h4y6@i.ua>
2023-07-04 10:32:48 +0300

    modified:   6c/controllers/books/getById.js,6c/controllers/books/updateById.js, 6c/helpers/index.js, 6c/routes/api/books.js new file:   6c/helpers/ctWr.js

--- 6c/controllers/books/updateById.js
+++ 6c/controllers/books/updateById.js
@@ -9,6 +9,9 @@ const updateById = async (req, res) => {
   }
   const { id } = req.params;
   const result = await Book.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw createError(404);
+  }
   res.json(result);
   next(error);
 };
--- 6c/routes/api/books.js
+++ 6c/routes/api/books.js
@@ -2,22 +2,22 @@ const express = require("express");
 
 const { basedir } = global;
 
-const { ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper, ctWr } = require("../../helpers");
 
 const ctrl = require(`${basedir}/controllers/books`);
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", ctWr(ctrl.getAll));
 
-router.get("/:id", ctrlWrapper(ctrl.getById));
+router.get("/:id", ctWr(ctrl.getById));
 
-router.post("/", ctrlWrapper(ctrl.add));
+router.post("/", ctWr(ctrl.add));
 
-router.put("/:id", ctrlWrapper(ctrl.updateById));
+router.put("/:id", ctWr(ctrl.updateById));
 
-router.patch("/:id/favorite", ctrlWrapper(ctrl.updateFavorite));
+router.patch("/:id/favorite", ctWr(ctrl.updateFavorite));
 
-router.delete("/:id", ctrlWrapper(ctrl.removeById));
+router.delete("/:id", ctWr(ctrl.removeById));
 
 module.exports = router;
