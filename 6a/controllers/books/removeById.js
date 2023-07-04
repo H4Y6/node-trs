b8981c8f3e573e72fa909c787d38eb629c231c05
Heorhii Chub <h4y6@i.ua>
2023-07-04 17:46:28 +0300

    modified:   6a/controllers/books/removeById.js, 6a/controllers/books/updateFavorite.js,6a/routes/api/books.js

--- 6a/controllers/books/removeById.js
+++ 6a/controllers/books/removeById.js
@@ -2,6 +2,17 @@ const { basedir } = global;
 const { Book } = require(`${basedir}/models/book`);
 const { createError } = require(`${basedir}/helpers`);
 
-const removeById = async (req, res, next) => {};
+const removeById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndRemove(id);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json({ message: "Book`s deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = removeById;
