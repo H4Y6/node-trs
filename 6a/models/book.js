91eb395b0d2ae3122bb65ee0aefa83d92efbbc51
Heorhii Chub <h4y6@i.ua>
2023-07-04 16:24:18 +0300

    modified: 6a/controllers/books/add.js, 6a/models/book.js

--- 6a/models/book.js
+++ 6a/models/book.js
@@ -2,13 +2,13 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const isbnRegexp = /^\d\d\d-\d-\d{3}-\d{5}-\d$/;
-// const values = ["fantastic", "science"]
+const values = ["fantastic", "science"];
 
 const bookSchema = new Schema(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
-    genres: { type: String, enum: ["fantastic", "science"], required: true },
+    genres: { type: String, enum: values, required: true },
     isbn: { type: String, match: isbnRegexp, required: true },
     favorite: { type: Boolean, default: false },
   },
@@ -18,7 +18,9 @@ const bookSchema = new Schema(
 const addSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
-  genres: Joi.string().valueOf("fantastic", "science").required(),
+  genres: Joi.string()
+    .valueOf(...values)
+    .required(),
   isbn: Joi.string().pattern(isbnRegexp).required(),
   favorite: Joi.boolean().default(false),
 });
