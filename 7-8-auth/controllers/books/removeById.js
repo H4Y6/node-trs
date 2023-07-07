088de5d057638853941a8b16aed15003361f550c
Heorhii Chub <h4y6@i.ua>
2023-07-07 22:04:46 +0300

    new dir:   7-8-auth

---
+++ 7-8-auth/controllers/books/removeById.js
@@ -0,0 +1,14 @@
+const { Book } = require("../../models/book");
+
+const { createError } = require("../../helpers");
+
+const removeById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Book.findByIdAndRemove(id);
+  if (!result) {
+    throw createError(404);
+  }
+  res.json({ message: "Message deleted" });
+};
+
+module.exports = removeById;
