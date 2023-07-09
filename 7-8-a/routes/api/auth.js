fc0fafb78b5f6096941abb47bc15d9dafaed25bc
Heorhii Chub <h4y6@i.ua>
2023-07-09 18:46:30 +0300

    new file:   7-8-a/controllers/auth/logout.js modified:   7-8-a/routes/api/auth.js, 7-8-a/controllers/auth/index.js

--- 7-8-a/routes/api/auth.js
+++ 7-8-a/routes/api/auth.js
@@ -10,5 +10,6 @@ const router = expess.Router();
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 module.exports = router;
