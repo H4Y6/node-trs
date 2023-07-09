aee4faeb57a257f7a15ed10f613ad7f68b177670
Heorhii Chub <h4y6@i.ua>
2023-07-09 18:13:18 +0300

    new file:   7-8-a/middlewares/auth.js, 7-8-a/controllers/auth/login.js, 7-8-a/middlewares/index.js modified:   7-8-a/models/book.js, 7-8-a/controllers/auth/index.js,7-8-a/controllers/books/add.js,7-8-a/controllers/books/getAll.js,  7-8-a/models/user.js,,7-8-a/routes/api/auth.js,7-8-a/routes/api/books.js,7-8-auth/controllers/books/getAll.js

--- 7-8-a/controllers/books/add.js
+++ 7-8-a/controllers/books/add.js
@@ -5,7 +5,8 @@ const { createError } = require(`${basedir}/helpers`);
 const add = async (req, res) => {
   const { error } = schemas.add.validate(req.body);
   if (error) throw createError(400, error.message);
-  const result = await Book.create(req.body);
+  const { id: owner } = req.user;
+  const result = await Book.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- 7-8-a/routes/api/auth.js
+++ 7-8-a/routes/api/auth.js
@@ -7,5 +7,6 @@ const ctrl = require(`${basedir}/controllers/auth`);
 const router = expess.Router();
 
 router.post("/register", ctrlWrapper(ctrl.register));
+router.post("/login", ctrlWrapper(ctrl.login));
 
 module.exports = router;
