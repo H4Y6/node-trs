d8e90894fe2ee08dcfc00c2be09a530e383c4384
Heorhii Chub <h4y6@i.ua>
2023-07-09 11:10:19 +0300

    new file:   7-8-auth/controllers/auth/getCurrent.js modified:   7-8-auth/controllers/auth/index.js,7-8-auth/routes/api/auth.js, 7-8-auth/routes/api/books.js

--- 7-8-auth/routes/api/auth.js
+++ 7-8-auth/routes/api/auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { basedir } = global;
 
-const ctrl = require("../../controllers/auth");
+const ctrl = require(`${basedir}/controllers/auth`);
 const { ctrlWrapper } = require(`${basedir}/helpers`);
+const { auth } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
+router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
 module.exports = router;
