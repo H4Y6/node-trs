a2f677f9e548699f69b05e2ae074f59f76ed9463
Heorhii Chub <h4y6@i.ua>
2023-07-10 15:27:06 +0300

    modified: 7-8b/controllers/auth/login.js, 7-8b/models/user.js

--- 7-8b/controllers/auth/login.js
+++ 7-8b/controllers/auth/login.js
@@ -1,3 +1,29 @@
-const login = async (req, res) => {};
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { basedir } = global;
+const { createError } = require(`${basedir}/helpers`);
+const { User, schemas } = require(`${basedir}/models/user`);
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { error } = schemas.login.validate(req.body);
+  if (error) {
+    throw createError(400, error.message);
+  }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createError(401, "Wrong email");
+  }
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    throw createError(401, "Wrong password");
+  }
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "22h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.json({ token });
+};
 
 module.exports = login;
