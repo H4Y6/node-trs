d2ca86e55e8e13dd1a30d23c0261689fe3d5c06e
Heorhii Chub <h4y6@i.ua>
2023-07-12 14:37:35 +0300

    modified:   7-8d/controllers/auth/index.js

--- 7-8d/controllers/auth/index.js
+++ 7-8d/controllers/auth/index.js
@@ -1,6 +1,6 @@
 const register = require("./register");
 const login = require("./login");
-const getCurrent = require("./getCurrent");
 const logout = require("./logout");
+const getCurrent = require("./getCurrent");
 
 module.exports = { register, login, getCurrent, logout };
