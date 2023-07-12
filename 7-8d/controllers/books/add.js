cf1f45128e2ed0e7855325316c2732adf23b62c6
Heorhii Chub <h4y6@i.ua>
2023-07-12 14:01:34 +0300

    modified:   7-8d/models/book.js, 7-8d/routes/api/books.js, 7-8d/controllers/books/add.js,  7-8d/controllers/books/getAll.js new files:   7-8d/middlewares/auth.js, 7-8d/middlewares/index.js

--- 7-8d/controllers/books/add.js
+++ 7-8d/controllers/books/add.js
@@ -1,13 +1,15 @@
 const { basedir } = global;
 const { createError } = require(`${basedir}/helpers`);
 const { Book, schemas } = require(`${basedir}/models/book`);
+const { auth } = require(`${basedir}/controllers/auth`);
 
 const add = async (req, res) => {
   const { error } = schemas.add.validate(req.body);
   if (error) {
     throw createError(400, error.message);
   }
-  const result = await Book.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Book.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
