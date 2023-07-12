31171a81f3794c636dfdaea3869e3e701ced28a0
Heorhii Chub <h4y6@i.ua>
2023-07-12 18:00:55 +0300

    new file:   7-8-auth-contacts/controllers/auth/logout.js modified:   7-8-auth-contacts/routes/api/users.js,  7-8-auth-contacts/controllers/auth/index.js

--- 7-8-auth-contacts/routes/api/users.js
+++ 7-8-auth-contacts/routes/api/users.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { basedir } = global;
 const { ctrlWrapper } = require(`${basedir}/helpers`);
 const ctrl = require(`${basedir}/controllers/auth`);
+const { auth } = require(`${basedir}/middlewares`);
 
 const router = express.Router();
 
 router.post("/register", ctrlWrapper(ctrl.register));
 router.post("/login", ctrlWrapper(ctrl.login));
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 module.exports = router;
