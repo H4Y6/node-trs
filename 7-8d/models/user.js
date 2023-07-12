2106ff23a68e133e5bf2c8927de1510a93c99a2b
Heorhii Chub <h4y6@i.ua>
2023-07-12 12:09:29 +0300

    modified:   7-8d/controllers/auth/register.js,7-8d/models/user.js

--- 7-8d/models/user.js
+++ 7-8d/models/user.js
@@ -1,11 +1,30 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-});
+const emailRegexp = /^\w+@\w+\.\w{2,3}$/;
+
+const userSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const User = model("user", userSchema);
 
-module.exports = { User };
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = { register: registerSchema, login: loginSchema };
+
+module.exports = { User, schemas };
