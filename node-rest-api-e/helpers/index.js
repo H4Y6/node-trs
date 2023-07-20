2210e2d418fc44445048dc758a2c35164f609737
H4Y6 <89087786+H4Y6@users.noreply.github.com>
2023-07-20 12:46:22 +0300

    Delete node-rest-api-e directory

--- node-rest-api-e/helpers/index.js
+++
@@ -1,7 +0,0 @@
-const createError = require("./createError");
-const ctrlWrap = require("./ctrlWrap");
-
-module.exports = {
-  createError,
-  ctrlWrap,
-};
--- node-rest-api-e/middlewares/index.js
+++
@@ -1,4 +0,0 @@
-const auth = require("./auth");
-const upload = require("./upload");
-
-module.exports = { auth, upload };
