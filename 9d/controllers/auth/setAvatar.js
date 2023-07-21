aed2eebb4ab3741dd76b10d72e9ee2cd05be932c
Heorhii Chub <h4y6@i.ua>
2023-07-21 20:25:41 +0300

    modified:   9d/controllers/auth/setAvatar.js

--- 9d/controllers/auth/setAvatar.js
+++ 9d/controllers/auth/setAvatar.js
@@ -10,13 +10,13 @@ const avatarsDir = path.join(basedir, "public", "avatars");
 const setAvatar = async (req, res) => {
   try {
     const { path: tempPath, originalname } = req.file;
-    const uploadPath = path.join(avatarsDir, originalname);
     const avatar = await jimp.read(tempPath);
     avatar.resize(250, 250).write(tempPath);
-    await fs.rename(tempPath, uploadPath);
     const { _id } = req.user;
     const [extention] = originalname.split(".").reverse();
     const newName = `${_id}.${extention}`;
+    const uploadPath = path.join(avatarsDir, newName);
+    await fs.rename(tempPath, uploadPath);
     const avatarURL = path.join("avatars", newName);
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({ avatarURL });
