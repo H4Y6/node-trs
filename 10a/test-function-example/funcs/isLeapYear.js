7f7545b7edb1d0fe53cfd9b4b31c61f742231969
Heorhii Chub <h4y6@i.ua>
2023-07-23 20:40:14 +0300

    modified:   10a/test-function-example/funcs/isLeapYear.js modified:   10a/test-function-example/funcs/isLeapYear.test.js

--- 10a/test-function-example/funcs/isLeapYear.js
+++ 10a/test-function-example/funcs/isLeapYear.js
@@ -1,16 +1,20 @@
 const isLeapYear = (year) => {
-  // if (year === undefined) {
-  //   throw new Error("year must exist");
-  // }
-  // if (typeof year !== "number") {
-  //   throw new Error("year must be number");
-  // }
-  // if (!Number.isInteger(year)) {
-  //   throw new Error("year must be integer");
-  // }
-  // if (year < 42) {
-  //   throw new Error("year must be 42 or more");
-  // }
+  if (year === undefined) {
+    throw new Error("year must exist");
+  }
+
+  if (typeof year !== "number") {
+    throw new Error("year must be number");
+  }
+
+  if (!Number.isInteger(year)) {
+    throw new Error("year must be integer");
+  }
+
+  if (year < 42) {
+    throw new Error("year must be 42 or more");
+  }
+
   const date = new Date(year, 2, 0);
   const days = date.getDate();
 
