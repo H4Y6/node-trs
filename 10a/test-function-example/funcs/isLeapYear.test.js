d2082ba7a1ff33e6173fc7051a737479f91c4269
Heorhii Chub <h4y6@i.ua>
2023-07-23 17:34:31 +0300

    new file:   10a/test-function-example/Button/Button.jsx new file:   10a/test-function-example/Button/index.js new file:   10a/test-function-example/Button/snapshots.js new file:   10a/test-function-example/__tests/.gitkeep new file:   10a/test-function-example/app.js new file:   10a/test-function-example/funcs/index.js new file:   10a/test-function-example/funcs/isLeapYear.js new file:   10a/test-function-example/funcs/isLeapYear.test.js modified:   package-lock.json modified:   package.json

---
+++ 10a/test-function-example/funcs/isLeapYear.test.js
@@ -0,0 +1,25 @@
+/*
+
+1. Получается целое положительное число.
+2. Возвращает true, если год высокосный, и false - если нет.
+3. Если получает данные в неправильном формате, выбрасывает ошибку
+с соответствующим текстом.
+
+2008 - true
+2003 - false
+1900 - false
+2000 - true
+
+41 - error 'year must be 42 or more'
+2008.4 - error 'year must be integer'
+() - error 'year must be exist'
+'2008' - error 'year must be number'
+null - error 'year must be number'
+false - error 'year must be number'
+true - error 'year must be number'
+()=> {} - error 'year must be number'
+{} - error 'year must be number'
+[] - error 'year must be number'
+*/
+
+const isLeapYear = require("./isLeapYear");
