149511b2b7df06ccce2b6900d99b0b18e39c6530
Heorhii Chub <h4y6@i.ua>
2023-07-24 16:53:41 +0300

    modified:   10/test-function-example/funcs/isLeapYear.test.js new file:   10a/test-func-example/index.html modified:   10a/test-func-example/isLeapYear.js modified:   10a/test-func-example/isLeapYear.test.js new file:   10a/test-func-example/package-lock.json new file:   10a/test-func-example/package.json modified:   10a/test-function-example/funcs/isLeapYear.test.js modified:   package-lock.json modified:   package.json

--- 10a/test-function-example/funcs/isLeapYear.test.js
+++ 10a/test-function-example/funcs/isLeapYear.test.js
@@ -1,7 +1,7 @@
 /*
 
 1. Получается целое положительное число.
-2. Возвращает true, если год высокосный, и false - если нет.
+2. Возвращает true, если год високосный, и false - если нет.
 3. Если получает данные в неправильном формате, выбрасывает ошибку
 с соответствующим текстом.
 
@@ -12,7 +12,7 @@
 
 41 - error 'year must be 42 or more'
 2008.4 - error 'year must be integer'
-() - error 'year must be exist'
+() - error 'year must exist'
 '2008' - error 'year must be number'
 null - error 'year must be number'
 false - error 'year must be number'
