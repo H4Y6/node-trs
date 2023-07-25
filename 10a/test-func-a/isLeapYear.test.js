6b483466c50f9032d4e666f673b35c7300200b6f
Heorhii Chub <h4y6@i.ua>
2023-07-25 11:44:03 +0300

    new dir:   10a/test-func-a

---
+++ 10a/test-func-a/isLeapYear.test.js
@@ -0,0 +1,22 @@
+/*
+1. Получается целое положительное число.
+2. Возвращает true, если год високосный, и false - если нет.
+3. Если получает данные в неправильном формате, выбрасывает ошибку
+с соответствующим текстом.
+
+2008 - true
+2003 - false
+1900 - false
+2000 - true
+
+41 - error 'year must be 42 or more'
+2008.4 - error 'year must be integer'
+() - error 'year must exist'
+'2008' - error 'year must be number'
+null - error 'year must be number'
+false - error 'year must be number'
+true - error 'year must be number'
+()=> {} - error 'year must be number'
+{} - error 'year must be number'
+[] - error 'year must be number'
+*/
